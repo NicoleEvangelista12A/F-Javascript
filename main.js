228c19c3224c771d93434d780cc7a7b01f24a5ee
Nicole <nicole.barross@eaportal.org>
2025-04-27 16:20:14 -0300

    segundo commit do java - segunda função

---
+++ main.js
@@ -0,0 +1,19 @@
+/*
+function mensagem() {
+    alert("Seja bem-vindo(a)!")
+    console.log("Seja bem-vindo(a)!")
+    console.log("Você é muito legal!")
+}
+
+mensagem ()
+*/
+
+function somar (numero1, numero2) {
+
+    console.log(numero1 + numero2)
+    }
+    
+    let numero1 = 20
+    let numero2 = 30
+    
+    somar (numero1, numero2)
