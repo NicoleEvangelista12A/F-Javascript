23ca6e30d7224e977331787e982501997b778491
Nicole <nicole.barross@eaportal.org>
2025-04-27 16:46:18 -0300

    terceiro commit do java - função four

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function mensagem() {
 mensagem ()
 */
 
-function somar (numero1, numero2) {
+/*function somar (numero1, numero2) {
 
     console.log(numero1 + numero2)
     }
@@ -17,3 +17,23 @@ function somar (numero1, numero2) {
     let numero2 = 30
     
     somar (numero1, numero2)
+*/
+
+function raizQuadrada (numero) {
+   console.log(Math.sqrt(numero))
+}
+    
+function potenciacao (valor1, valor2) {
+    console.log(Math.pow(valor1, valor2))
+}
+    
+function multiplicacao(valor1, valor2) {
+    console.log(valor1 * valor2)
+}
+    
+let numero1 = Number(prompt("Digite o número 1:"))
+let numero2 = Number(prompt("Digite o número 2:"))
+    
+raizQuadrada (numero1)
+potenciacao (numero1, numero2)
+multiplicacao (numero2, numero1)
